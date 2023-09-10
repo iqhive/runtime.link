f4f66c048c9203579c804fa07c7386f9049582ce
Quentin Quaadgras <qquaadgras@iqhive.com>
2023-09-10 22:29:21 +1200

    just use std cat for now

--- std/gnu/gnu_test.go
+++ std/gnu/gnu_test.go
@@ -5,7 +5,6 @@ import (
 	"testing"
 
 	"runtime.link/std"
-	"runtime.link/std/gnu"
 	"runtime.link/std/posix"
 )
 
@@ -13,14 +12,14 @@ func TestCore(t *testing.T) {
 	ctx := context.Background()
 
 	var cmd struct {
-		cat gnu.Cat
+		cat posix.CatCommand
 	}
 	if err := std.Link(&cmd); err != nil {
 		t.Fatal(err)
 	}
 
-	var opts = gnu.CatOptions{
-		LineNumbers: true,
+	var opts = posix.CatOptions{
+		//LineNumbers: true,
 	}
 	if err := cmd.cat.Files(ctx, posix.Paths{"gnu_test.go"}, &opts); err != nil {
 		t.Fatal(err)
