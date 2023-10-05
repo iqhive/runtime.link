78f21b0ef3c7c616cd958fb36be1df29c093364c
Quentin Quaadgras <qquaadgras@iqhive.com>
2023-10-06 11:42:01 +1300

    add txt.Format methods to human types.

--- ref/std/human/human.go
+++ ref/std/human/human.go
@@ -1,8 +1,37 @@
 // Package human provides standard way to reference people by name.
 package human
 
+import (
+	"fmt"
+	"unicode"
+
+	"runtime.link/txt"
+)
+
 // Name refers to a person by their name.
-type Name string
+type Name = txt.Is[name]
+
+type name struct{}
+
+func (name) Parse(text string) (name, error) {
+	for _, char := range text {
+		if !unicode.IsLetter(char) && char != ' ' {
+			return name{}, fmt.Errorf("invalid character in name: %q", char)
+		}
+	}
+	return name{}, nil
+}
 
 // Readable annotates a string as being readable by a person.
-type Readable string
+type Readable = txt.Is[readable]
+
+type readable struct{}
+
+func (readable) Parse(text string) (readable, error) {
+	for _, char := range text {
+		if unicode.IsPrint(char) {
+			return readable{}, fmt.Errorf("unreadable character: %q", char)
+		}
+	}
+	return readable{}, nil
+}
