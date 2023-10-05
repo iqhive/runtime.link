9b2b5a7cbab895fd58a4410dd71006e520fb795d
Quentin Quaadgras <qquaadgras@iqhive.com>
2023-10-06 12:04:32 +1300

    add cash package.

---
+++ xyz/std/cash/cash.go
@@ -0,0 +1,11 @@
+// Package cash provides a standard way to represent cash amounts using the smallest unit of currency.
+package cash
+
+// Credit amount in the smallest unit of currency.
+type Credit int64
+
+// Debit amount in the smallest unit of currency.
+type Debit int64
+
+// Balance amount in the smallest unit of currency.
+type Balance int64
