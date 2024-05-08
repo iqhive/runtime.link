c65aa7757eb15494842729d3ddc3e958ac551f18
Quentin Quaadgras <qquaadgras@iqhive.com>
2024-05-09 09:48:15 +1200

    api/rest: add TestFallback.

--- api/rest/rest_test.go
+++ api/rest/rest_test.go
@@ -5,6 +5,7 @@ import (
 	"net/http/httptest"
 	"testing"
 
+	"github.com/gorilla/mux"
 	"runtime.link/api"
 	"runtime.link/api/rest"
 	"runtime.link/xyz"
@@ -42,5 +43,52 @@ func TestErrors(t *testing.T) {
 	}
 	if resp.Body.String() != "access denied\n" {
 		t.Errorf("got %q, want %q", resp.Body.String(), "access denied\n")
+	}
+}
+
+// TestFallback demonstrates how APIs can be composed.
+func TestFallback(t *testing.T) {
+
+	type API1 struct {
+		api.Specification
+
+		DoSomething func() string `rest:"POST /do-something"`
+	}
+
+	type API2 struct {
+		api.Specification
+
+		DoSomethingElse func() string `rest:"POST /do-something-else"`
+	}
+
+	var Handler1, _ = rest.Handler(nil, API1{
+		DoSomething: func() string { return "DoSomething" },
+	})
+	var Handler2, _ = rest.Handler(nil, API2{
+		DoSomethingElse: func() string { return "DoSomethingElse" },
+	})
+
+	router := (Handler1.(*mux.Router))
+	router.NotFoundHandler = Handler2
+
+	req := httptest.NewRequest("POST", "/do-something-else", nil)
+	rec := httptest.NewRecorder()
+	router.ServeHTTP(rec, req)
+	if rec.Code != 200 || rec.Body.String() != `"DoSomethingElse"` {
+		t.Fatal("unexpected body")
+	}
+
+	req = httptest.NewRequest("POST", "/do-something", nil)
+	rec = httptest.NewRecorder()
+	router.ServeHTTP(rec, req)
+	if rec.Code != 200 || rec.Body.String() != `"DoSomething"` {
+		t.Fatal("unexpected body")
+	}
+
+	req = httptest.NewRequest("POST", "/1234", nil)
+	rec = httptest.NewRecorder()
+	router.ServeHTTP(rec, req)
+	if rec.Code != 404 {
+		t.Fatal("unexpected body")
 	}
 }
