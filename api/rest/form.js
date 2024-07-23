151addd4dcc9ec8e3c144c9e82c67493321f9db8
Quentin Quaadgras <qquaadgras@iqhive.com>
2024-07-23 15:22:24 +1200

    add form.js

---
+++ api/rest/form.js
@@ -0,0 +1,26 @@
+let req = new XMLHttpRequest();
+req.onreadystatechange = function () {
+  if (req.readyState === 4 && req.status === 200) {
+    let schema = JSON.parse(req.responseText);
+    $("form").jsonForm({
+      schema: schema,
+      onSubmit: function (errors, values) {
+        $.ajax({
+          type: "POST",
+          url: "",
+          data: JSON.stringify(values),
+          contentType: "application/json",
+          success: function () {
+            alert("Success!");
+          },
+          error: function () {
+            alert("Error: ${xhr.status}");
+          },
+        });
+      },
+    });
+  }
+};
+req.open("GET", "", true);
+req.setRequestHeader("Accept", "application/json");
+req.send();
