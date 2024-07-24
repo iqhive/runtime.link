54cf9200515d52a4e9fe23f80739704359113b02
Quentin Quaadgras <qquaadgras@iqhive.com>
2024-07-25 10:44:11 +1200

    api/rest: show form request result in response.

--- api/rest/form.js
+++ api/rest/form.js
@@ -1,29 +1,34 @@
-let http = async function (method, accept, path, payload) {
-  try {
-    let response = await fetch(path, {
-      method: method,
-      headers: {
-        Accept: accept,
-        "Content-Type": "application/json",
-      },
-      body: payload,
-    });
-    return response.json();
-  } catch (error) {
-    alert(error);
-  }
-};
-let schema = await http("GET", "application/schema+json", "");
-console.log(schema);
-$("form").jsonForm({
-  schema: schema,
-  onSubmit: async function (errors, values) {
-    let response = await http(
-      "POST",
-      "application/json",
-      "",
-      JSON.stringify(values),
-    );
-    $("pre").text(JSON.stringify(response, null, 2));
-  },
-});
+(async function () {
+  let http = async function (method, accept, path, payload) {
+    try {
+      let response = await fetch(path, {
+        method: method,
+        headers: {
+          Accept: accept,
+          "Content-Type": "application/json",
+        },
+        body: payload,
+      });
+      return response.json();
+    } catch (error) {
+      alert(error);
+    }
+  };
+  let schema = await http("GET", "application/schema+json", "");
+  $("form").jsonForm({
+    schema: schema,
+    onSubmit: async function (errors, values) {
+      let response = await http(
+        "POST",
+        "application/json",
+        "",
+        JSON.stringify(values),
+      );
+      $("pre").text(JSON.stringify(response, null, 2));
+      $("pre").css("display", "block");
+    },
+  });
+  let resource = await http("GET", "application/json", "");
+  $("pre").text(JSON.stringify(resource, null, 2));
+  $("pre").css("display", "block");
+})();
