2a119137f207336df0465d97692db87eb2b6d647
Quentin Quaadgras <qquaadgras@iqhive.com>
2024-08-14 15:01:42 +1200

    fix

--- api/rest/link.go
+++ api/rest/link.go
@@ -266,6 +266,7 @@ func link(client *http.Client, spec specification, host string) error {
 
 				//We are expecting JSON.
 				req.Header.Add("Accept", "application/json")
+				req.Header.Add("Content-Type", contentType)
 				if debug {
 					fmt.Println("headers:\n", req.Header)
 				}
