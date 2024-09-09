d521e77f8b6a59f6cde2cf68ebfde05271b90350
Quentin Quaadgras <qquaadgras@iqhive.com>
2024-09-10 11:37:01 +1200

    location, not postal

--- pii/codes/codes.go
+++ pii/codes/codes.go
@@ -3,8 +3,8 @@ package codes
 import "runtime.link/pii"
 
 type (
-	// Postal code, such as a ZIP code or a postal code.
-	Postal pii.String
+	// Location code, such as a ZIP code, area code or a postal code.
+	Location pii.String
 
 	// Identity code, such as a social security number, license number or taxpayer identifier.
 	Identity pii.String
