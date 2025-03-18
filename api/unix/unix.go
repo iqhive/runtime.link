31b42c96c6d2593a4017d28715398c5da2dea224
Quentin Quaadgras <email@quentinquaadgras.com>
2025-03-19 11:32:16 +1300

    api/unix: add Nanos and Path types

--- api/unix/unix.go
+++ api/unix/unix.go
@@ -2,3 +2,9 @@ package unix
 
 // Seconds since the Unix epoch.
 type Seconds int64
+
+// Nanos are the number of nanoseconds since the Unix epoch.
+type Nanos int64
+
+// Path to a file.
+type Path string
