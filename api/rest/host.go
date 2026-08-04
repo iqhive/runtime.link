b5df758c0db399e69652d597af168d1e461e46ed
Relihan Myburgh <github@qoke.net>
2026-08-04 11:21:09 +0000

    Escape example category headings in examples page HTML

--- api/rest/host.go
+++ api/rest/host.go
@@ -611,7 +611,7 @@ func Handlers(auth api.Auth[*http.Request], impl any, param_format, remainder_fo
 						} else {
 							fmt.Fprintf(w, "<details class=\"example-category\">")
 						}
-						fmt.Fprintf(w, "<summary class=\"category-header\">%s</summary>", formatExampleCategory(category))
+						fmt.Fprintf(w, "<summary class=\"category-header\">%s</summary>", html.EscapeString(formatExampleCategory(category)))
 						fmt.Fprintf(w, "<div class=\"category-examples\">")
 						for _, exampleName := range categoryExamples {
 							title := formatExampleCategory(exampleName)
